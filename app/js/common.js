10dda5c82f32b07bf17985f37bfc3b429fa944a0
Viktoria <zaharova181114@gmail.com>
2021-01-17 15:27:55 +0200

    product

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,7 @@
+// mask phone
 $('[name="phone"]').mask('+7(999) 999-99-99');
 
+// mobile menu
 $('.btn-burger').on("click", function () {
     $('.mobile-menu').fadeToggle();
 });
@@ -8,6 +10,7 @@ $('.mobile-menu .btn-close').on('click', function () {
    $('.mobile-menu').fadeOut();
 });
 
+// slider range price
 $('.slider-range').slider({
     range: true,
     min: 0,
@@ -27,6 +30,7 @@ $('.slider-range').slider({
 $(".dec1").val($(".slider-range").slider("values", 0) + ' ' + 'руб');
 $(".dec2").val($(".slider-range").slider("values", 1) + ' ' + 'руб');
 
+// filter
 $('.btn-filter').on("click", function (e) {
     e.preventDefault();
    $('.sidebar').fadeToggle();
@@ -37,13 +41,13 @@ $('.sidebar .btn-close').on("click", function () {
 });
 
 
+// загрузка скрытых блоков
 $('.catalog-content > .row').each(function () {
     if ($(this).find('.card-product-col').length > 6) {
         $(this).find('.card-product-col').slice(6).hide();
     }
 
 });
-
 
 $('.btn-load').on('click', function (e) {
     e.preventDefault();
@@ -53,4 +57,45 @@ $('.btn-load').on('click', function (e) {
     if(onBlock <= 0) {
         $('.btn-load').hide();
     }
+});
+
+// gallery photo product
+$(function () {
+    let penImg = $('.products-gallery__max img');
+    let linksImg = $('.products-gallery__max');
+
+    $('.products-gallery-min').on('click', function () {
+        $('.products-gallery-min').removeClass('click-item');
+        $(this).addClass('click-item');
+        let imgPath;
+
+        imgPath = $(this).attr('data-img-path');
+
+        penImg.attr('src', imgPath);
+        linksImg.attr('href', imgPath);
+
+    });
+});
+
+// =/- цена
+$('.down').click(function () {
+    var $input = $(this).parent().find('input');
+    var count = parseInt($input.val()) - 1;
+    count = count < 1 ? 1 : count;
+    $input.val(count);
+    $input.change();
+    return false;
+});
+$('.up').click(function () {
+    var $input = $(this).parent().find('input');
+    $input.val(parseInt($input.val()) + 1);
+    $input.change();
+    return false;
+});
+
+// tabs
+$('ul.tabs__caption').on('click', 'li:not(.active)', function () {
+    $(this)
+        .addClass('active').siblings().removeClass('active')
+        .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
 });
