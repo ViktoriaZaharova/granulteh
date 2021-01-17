afbc50065a05e0ea7452a658c6680e69abd79c16
Viktoria <zaharova181114@gmail.com>
2021-01-17 16:30:27 +0200

    mobile product

--- app/js/common.js
+++ app/js/common.js
@@ -99,3 +99,14 @@ $('ul.tabs__caption').on('click', 'li:not(.active)', function () {
         .addClass('active').siblings().removeClass('active')
         .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
 });
+
+// animate scroll
+$('.go_to').click(function () {
+    var scroll_el = $(this).attr('href');
+    if ($(scroll_el).length != 0) {
+        $('html, body').animate({
+            scrollTop: $(scroll_el).offset().top
+        }, 500);
+    }
+    return false;
+});
