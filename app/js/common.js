d8ac913ce370eb0e4ade94058e95d1cc25135ef2
Viktoria <zaharova181114@gmail.com>
2021-01-17 07:54:33 +0200

    media home

--- app/js/common.js
+++ app/js/common.js
@@ -1,1 +1,9 @@
 $('[name="phone"]').mask('+7(999) 999-99-99');
+
+$('.btn-burger').on("click", function () {
+    $('.mobile-menu').fadeToggle();
+});
+
+$('.mobile-menu .btn-close').on('click', function () {
+   $('.mobile-menu').fadeOut();
+});
